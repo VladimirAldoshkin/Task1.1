c3adfd09c5fa422d96cc50d94674e5fb525b410e
VladimirAldoshkin <animag06@mail.ru>
2024-03-30 17:19:12 +0300

    Lesson1:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum1 = i1 + i2;
+Console.WriteLine(sum1);
+
+long l1 = 6;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+int produkt1 = i1 * i2;
+long produkt2 = l1 * i1;
+double produkt3 = i1 * d1;
+Console.WriteLine(produkt1);
+Console.WriteLine(produkt2);
+Console.WriteLine(produkt3);
